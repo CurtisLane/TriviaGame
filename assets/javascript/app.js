844a8c86a564cc951963c2bcc38376abb4ec1942
Curtis Lane <clanelmt@gmail.com>
2020-04-22 00:20:10 -0400

    Add pseudo code, reset.css. Add index.html boilerplate, links and scripts.

---
+++ assets/javascript/app.js
@@ -0,0 +1,23 @@
+// Document ready function
+
+// Global Variables
+const domOne = $('#id')
+const domOne = $('#id')
+const domOne = $('#id')
+const domOne = $('#id')
+const domOne = $('#id')
+const domOne = $('#id')
+const restartButton = $('#restartButton')
+
+// Questions object with 3 tiers(book 1,2,3) of 10 questions each.
+// Each tier connects with one third of gameplay
+// player is graded based on correct guesses and evaluated for fandom level
+// each tier resets number of wrong guesses
+// timer counts down while question and answers are posted
+// if correct answer is chosen, a page saying correct with an uncle ozai quote appears 
+// after a few seconds the page goes away and a new question is posted
+// if incorrect answer is chosen, a page displays the correct answer with zuko saying that's rough buddy
+// if time runs out, you get out of page dialogue on the page with a graphic
+// once finished, player game stats are displayed
+// after game is finished, start over button appears
+// start over button does not relaod the page
